27c88c39a39f9206da64666480847e1e1ad5f181
anymuse <anymuse@gmail.com>
2021-04-21 17:27:45 +0800

    Make get method static

--- lib/mirror_mac_address.dart
+++ lib/mirror_mac_address.dart
@@ -4,19 +4,18 @@ import 'package:device_info/device_info.dart';
 import 'package:flutter/services.dart';
 
 class MirrorMacAddress {
+  MirrorMacAddress._();
+
   static const MethodChannel _channel =
       const MethodChannel('mirror_mac_address');
 
   static const _mirror_model = 'm43c01';
 
-  String _mac;
-
   /// Get MAC address from Mirror.
   ///
   /// Return an empty [String] if the device is not a Mirror.
-  Future<String> get() async => _mac ??= await _get();
 
-  Future<String> _get() async {
+  static Future<String> get get async {
     try {
       final deviceInfo = DeviceInfoPlugin();
       final androidInfo = await deviceInfo.androidInfo;
